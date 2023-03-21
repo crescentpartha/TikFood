5ff136e6a2610e1c23e6fea82e4e50f693f78691
crescentpartha <crescentpartha406@gmail.com>
2023-03-22 01:12:19 +0600

    Reservation section added

--- tikfood/public/Steps.md
+++ tikfood/public/Steps.md
@@ -3,6 +3,7 @@
 - [Fix `Tailwind CSS` and `daisyUI` ___setup___ problem using `ChatGPT`](#fix-tailwind-css-and-daisyui-setup-problem-using-chatgpt)
   - [Tailwind CSS does not work for create react app. how to setup properly `(ChatGPT - Working)`](#tailwind-css-does-not-work-for-create-react-app-how-to-setup-properly-chatgpt---working)
   - [how to Install daisyUI as a Tailwind CSS plugin `(ChatGPT - Working)`](#how-to-install-daisyui-as-a-tailwind-css-plugin-chatgpt---working)
+  - [Fix the upstream dependency conflict installing NPM packages](#fix-the-upstream-dependency-conflict-installing-npm-packages)
 
 # Fix `Tailwind CSS` and `daisyUI` ___setup___ problem using `ChatGPT`
 
@@ -119,5 +120,17 @@ ReactDOM.render(
 );
 ```
 > With these steps, DaisyUI should be set up and working properly as a Tailwind CSS plugin. <br> You can now use DaisyUI components in your React application by following its documentation.
+
+**[🔼Back to Top](#table-of-contents)**
+
+## Fix the upstream dependency conflict installing NPM packages
+
+> `How to fix the upstream dependency conflict installing NPM packages:` <br /><br /> `npm install '--legacy-peer-deps' -S <whatever package>` <br /># `-S is the same as --save` <br /># `i.e` <br />`npm install '--legacy-peer-deps' --save react-useanimations` <br />[stackoverflow solution](https://stackoverflow.com/questions/64936044/fix-the-upstream-dependency-conflict-installing-npm-packages)
+
+``` JavaScript
+// Install Font Awesome Icons
+
+npm install '--legacy-peer-deps' --save @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+```
 
 **[🔼Back to Top](#table-of-contents)**
--- tikfood/src/components/Reservation/Reservation.js
+++ tikfood/src/components/Reservation/Reservation.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import restaurent_book from '../../images/reservation/restaurant_book.jpg';
+import photo1 from '../../images/reservation/1.jpg';
+import photo2 from '../../images/reservation/2.jpg';
+import photo3 from '../../images/reservation/3.jpg';
+import photo4 from '../../images/reservation/4.jpg';
 
 const Reservation = () => {
     return (
-        <div>
-            <h2>Reservation Page</h2>
+        <div className='mt-10 px-5 md:px-20'>
+            <h2 className='text-5xl mt-5 mb-8 text-center'>Reservation</h2>
+            <div className="grid md:grid-cols-1 xl:grid-cols-2 items-center justify-center bg-base-200 py-10 my-2 rounded">
+                <img src={restaurent_book} alt='Reserve your seat' className="w-full p-2 rounded-lg shadow-2xl" />
+                <div className='px-5 md:mt-5'>
+                    <h1 className="text-4xl font-semibold text-red-500">Welcome</h1>
+                    <p className="py-6 font-semibold text-2xl xl:text-4xl">A Place of Tasty Food with Sylheti Styles</p>
+                    <Link to='tel:+8801723242526'><FontAwesomeIcon icon={faPhone} className='text-xl lg:text-3xl text-gray-300 p-3 lg:p-4 rounded-full bg-green-600' /></Link>
+                    <p className="py-6 font-semibold text-2xl xl:text-4xl">Book a table at Our Restaurant simply Call us on <span className='text-blue-500'><Link to='tel:+8801723242526'>+8801723242526</Link></span></p>
+                </div>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mb-5 justify-items-center'>
+                <img className='w-max-96 h-60 h-min-60 rounded' src={photo1} alt="Restaurant" />
+                <img className='w-max-96 h-60 h-min-60 rounded' src={photo2} alt="Restaurant" />
+                <img className='w-max-96 h-60 h-min-60 rounded' src={photo3} alt="Restaurant" />
+                <img className='w-max-96 h-60 h-min-60 rounded' src={photo4} alt="Restaurant" />
+            </div>
         </div>
     );
 };
