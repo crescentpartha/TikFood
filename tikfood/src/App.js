18a3bd54250b93ffc2b58d17257057d6fef31135
crescentpartha <crescentpartha406@gmail.com>
2023-03-21 14:03:31 +0600

    Setup routes and Header component

--- tikfood/src/App.js
+++ tikfood/src/App.js
@@ -5,6 +5,10 @@ import Register from './components/Authentication/Register';
 import Login from './components/Authentication/Login';
 import NotFound from './components/Shared/NotFound';
 import Header from './components/Shared/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import Reservation from './components/Reservation/Reservation';
+import Gallery from './components/Gallery/Gallery';
+import About from './components/AboutUS/About';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/reservation' element={<Reservation></Reservation>}></Route>
+        <Route path='/gallery' element={<Gallery></Gallery>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
--- tikfood/src/components/Home/Home.js
+++ tikfood/src/components/Home/Home.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2 className='text-3xl text-green-500 font-bold underline'>Home Container</h2>
-            <button className="btn btn-primary">Button</button>
-            <button className="btn btn-outline btn-warning">Warning</button>
+            <h2 className='text-3xl text-green-500 font-bold'>Home Container</h2>
         </div>
     );
 };
