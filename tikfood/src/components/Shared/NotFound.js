64ab58eea796b1a074db63b0c37d51addd4c1b9b
crescentpartha <crescentpartha406@gmail.com>
2023-03-21 16:55:07 +0600

    Not Found page added

--- tikfood/src/components/Shared/NotFound.js
+++ tikfood/src/components/Shared/NotFound.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found Page</h2>
+        <div className='py-20'>
+            <h1 className='font-bold text-9xl text-slate-700'>404</h1>
+            <h3 className='font-bold text-4xl text-slate-600'>Not Found</h3> <br />
+            <p className='font-semibold text-md'>The resource requested could not be found on this server!</p>
         </div>
     );
 };
