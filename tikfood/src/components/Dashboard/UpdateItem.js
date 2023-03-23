e1e5e982472ca8a6c1bcaefe5c192f29d06aa3ac
crescentpartha <crescentpartha406@gmail.com>
2023-03-23 19:00:54 +0600

    Design Update Menu Item

---
+++ tikfood/src/components/Dashboard/UpdateItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const UpdateItem = ({ menu }) => {
+    const { name, price, availability, img } = menu;
+
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-16 h-16">
+                            <img src={img} alt={name} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{name}</div>
+                        <div className="text-sm opacity-50">${price}</div>
+                        <div>
+                            {
+                                availability === "true"
+                                ? <div className="text-sm opacity-50">Available</div>
+                                : <div className="text-sm opacity-50 text-red-500">Not Available</div>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td><Link to='#' className='text-blue-400 font-normal'>Update</Link></td>
+            <td><Link to='#' className='text-blue-400 font-normal'>Delete</Link></td>
+        </tr>
+    );
+};
+
+export default UpdateItem;
--- tikfood/src/components/Dashboard/UpdateMenu.js
+++ tikfood/src/components/Dashboard/UpdateMenu.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import useMenus from '../../hooks/useMenus';
+import UpdateItem from './UpdateItem';
 
 const UpdateMenu = () => {
+    const [menus] = useMenus();
+
     return (
         <div>
-            <h2>Update a Menu</h2>
+            <h2 className='text-2xl font-bold text-gray-500 mb-4 text-left'>Update a Menu</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            menus.map(menu => <UpdateItem
+                                key={menu._id}
+                                menu={menu}
+                            ></UpdateItem>)
+                        }                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
